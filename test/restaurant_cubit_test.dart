92862188ad2e7e8b02820362e9b17ec54e952cb7
Randytia Akbar <randytia.akbar@gmail.com>
2023-05-02 10:39:03 +0700

    restaurant cubit test

--- test/restaurant_cubit_test.dart
+++ test/restaurant_cubit_test.dart
@@ -1,21 +1,36 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mama_resto/features/restaurant/cubit/restaurant_cubit.dart';
-// import 'package:mama_resto/features/restaurant/data/repositories/restaurant_repository_impl.dart';
-// import 'package:mama_resto/features/restaurant/domain/repositories/restaurant_repository.dart';
-// import 'package:mockito/mockito.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mama_resto/features/restaurant/cubit/restaurant_cubit.dart';
+import 'package:mama_resto/features/restaurant/domain/entities/get_list_restaurant_params.dart';
+import 'package:bloc_test/bloc_test.dart';
 
-// class MockRestaurantRepository extends Mock implements RestaurantRepository {}
+import 'package:mama_resto/features/restaurant/domain/usecases/get_list_restaurant.dart';
+import 'package:mockito/mockito.dart';
 
-// class MockRestaurantRepositoryImpl extends Mock
-//     implements RestaurantRepositoryImpl {}
+class MockGetListRestaurant extends Mock implements GetListRestaurant {}
 
-// void main() {
-//   RestaurantCubit? restaurantCubit;
-//   MockRestaurantRepository? mockRestaurantRepository;
-//   MockRestaurantRepositoryImpl? mockRestaurantRepositoryImpl;
+void main() {
+  MockGetListRestaurant mockGetListRestaurant;
+  RestaurantCubit restaurantCubit;
 
-//   setUp(() {
-//     restaurantCubit =
-//         RestaurantCubit(restaurantRepository: mockRestaurantRepositoryImpl);
-//   });
-// }
+  mockGetListRestaurant = MockGetListRestaurant();
+  restaurantCubit = RestaurantCubit(listRestaurant: mockGetListRestaurant);
+  setUp(() {});
+
+  tearDown(() => restaurantCubit.close());
+
+  test('bloc should have initial state as [RestaurantInitial]',
+      () => expect(restaurantCubit.state.runtimeType, RestaurantInitial));
+
+  blocTest(
+    'should emit [RestaurantLoading, RestaurantLoaded] state when data loaded',
+    build: () => restaurantCubit,
+    act: (cubit) {
+      when(mockGetListRestaurant.call(const GetListRestaurantParams(query: '')))
+          .thenAnswer((_) async => const Right([]));
+
+      cubit.fetchRestaurant(query: '');
+    },
+    expect: () => [isA<RestaurantLoading>(), isA<RestaurantLoaded>()],
+  );
+}
