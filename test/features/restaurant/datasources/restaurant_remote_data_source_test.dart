e39a16baecbe6c46e275f46feec96f4f4711360e
Randytia Akbar <randytia.akbar@gmail.com>
2023-05-02 11:58:41 +0700

    test post

--- test/features/restaurant/datasources/restaurant_remote_data_source_test.dart
+++ test/features/restaurant/datasources/restaurant_remote_data_source_test.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mama_resto/features/restaurant/data/datasources/restaurant_remote_data_source.dart';
+import 'package:mama_resto/features/restaurant/data/models/customer_review.dart';
 import 'package:mama_resto/features/restaurant/data/models/restaurant.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -69,6 +70,41 @@ void main() {
               categories: [],
               customerReviews: [],
               menus: null)
+        ]);
+      });
+    });
+  });
+
+  group('Post method', () {
+    test('can be used to get responses for any requests', () async {
+      final responsepayload = jsonEncode({
+        "error": false,
+        "message": "success",
+        "customerReviews": [
+          {"name": "Budi", "review": "Makanan Enak", "date": "2 Mei 2023"},
+        ]
+      });
+
+      final httpResponse = ResponseBody.fromString(
+        responsepayload,
+        200,
+        headers: {
+          Headers.contentTypeHeader: [Headers.jsonContentType],
+        },
+      );
+
+      when(mockHttpClientAdapter.fetch(any, any, any))
+          .thenAnswer((_) async => httpResponse);
+
+      final result =
+          await remoteDataSource.addReview('1', 'Budi', 'Makanan Enak');
+
+      result.fold((l) {
+        expect(l, 'success');
+      }, (r) {
+        expect(r, [
+          const CustomerReview(
+              date: '2 Mei 2023', name: 'Budi', review: 'Makanan Enak')
         ]);
       });
     });
